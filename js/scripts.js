7bb24bae97be2b68b0b24e63fdb0a52d488fc6ba
Afton Downey and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-01-12 09:40:15 -0800

    Add js to toggle images

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,36 @@
 $(document).ready(function() {
 
   $("button#hello").click(function() {
-    $("ul#cat").prepend("<li>Hello!</li>");
-    $("ul#dog").append("<li>Well, Hello there.</li>")
+    $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#dog").append("<li>Woof!</li>");
+
+    $("li").click(function() {
+      $("img#catpic").slideDown();
+    });
+    $("img#catpic").click(function() {
+      $(this).toggle();
+      // body...
+    });
   });
 
   $("button#goodbye").click(function() {
-    $("ul#cat").prepend("<li>Goodbye!</li>");
-    $("ul#dog").append("<li>So long!</li>")
+    $("ul#cat").prepend("<li>Meow Meow</li>");
+    $("ul#dog").append("<li>Bark bark bark!</li>");
+
+    $("li").click(function() {
+      $("img#dogpic").slideDown();
+    });
+    $("img#dogpic").click(function() {
+      $(this).toggle();
+      // body...
+    });
+
   });
 
-  $("button#stop").click(function() {
-    $("ul#cat").prepend("<li>Stop copying me!</li>");
-    $("ul#dog").append("<li>Sorry, imitiation is a form of flattery.</li>")
-  });
+  // $("button#stop").click(function() {
+  //   $("ul#cat").prepend("<li>Stop copying me!</li>");
+  //   $("ul#dog").append("<li>Sorry, imitiation is a form of flattery.</li>")
+  // });
 
 
 });
